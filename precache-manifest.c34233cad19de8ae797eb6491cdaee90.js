11bd38301534d705d57ede5e1af491a7b8ac8ca1
Sergio <sergiiy@tallium.com>
2020-04-27 01:49:28 +0300

    Updates

---
+++ precache-manifest.c34233cad19de8ae797eb6491cdaee90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "069f37cbeed5d8c9eb2995a8559cf81a",
+    "url": "/react-weather-page/index.html"
+  },
+  {
+    "revision": "e45f33ba6487802ced83",
+    "url": "/react-weather-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a6179e8060a54e8752b9",
+    "url": "/react-weather-page/static/js/2.e4e3f8e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-page/static/js/2.e4e3f8e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e45f33ba6487802ced83",
+    "url": "/react-weather-page/static/js/main.7aeb9d6a.chunk.js"
+  },
+  {
+    "revision": "9f6af3305b2256ef2d10",
+    "url": "/react-weather-page/static/js/runtime-main.6b87fae0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-weather-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
