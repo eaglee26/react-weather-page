c733a81669f323d8f6c4e36d681a5c48c3c3c5ae
Sergio <sergiiy@tallium.com>
2020-07-06 16:01:20 +0300

    test 2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Go to coding and after sleep
+          Go to coding and after sleep lol
         </p>
         <a
           className="App-link"
